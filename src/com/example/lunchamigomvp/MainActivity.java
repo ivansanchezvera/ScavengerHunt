268b7fa64b4b66877df4946bdd0b7f0f460ec113
Denis Thamrin <Denisthamrin@gmail.com>
2014-05-04 11:00:53 +0700

    First Commit

---
+++ src/com/example/lunchamigomvp/MainActivity.java
@@ -0,0 +1,159 @@
+package com.example.lunchamigomvp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+// abstract this out to a different package
+// todo
+// what to do on Pause or back button ?
+// todo
+// todo
+// use regex
+public class MainActivity extends ActionBarActivity {
+	/* login */
+	
+	private String mEmail;
+	private String mPassword;
+	
+	private EditText PasswordView;
+	private EditText EmailView;	
+	private Boolean isLoggedIn;
+	private Boolean isValid;
+	
+	SharedPreferences sharedpreferences;
+	
+	public static final String AUTHPREFS = "authPrefs" ;
+	public static final String EMAIL = "emailKey"; 
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		isLoggedIn = false;
+		isValid = false;
+		EmailView = (EditText) findViewById(R.id.editEmailAddress);		
+		PasswordView = (EditText) findViewById(R.id.editPassword);
+		
+	    sharedpreferences = getSharedPreferences(AUTHPREFS, Context.MODE_PRIVATE);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	// Login Button
+	public void tryLogin(View view) {
+
+		mEmail = EmailView.getText().toString();
+		mPassword = PasswordView.getText().toString();
+				
+		attemptLogin();
+		
+		// start another activity if the person succeed to login
+		if (isLoggedIn == true) {
+			storeCredentials();
+			Intent intent = new Intent(this, ShakerActivity.class);
+			startActivity(intent);
+		}
+	}
+	
+
+	
+	public void attemptLogin() {
+		// Please abstract the string to R.strings
+		// todo
+
+		//shows error in a toast
+		// todo improve this to be similar to LoginActivity android template
+		// or just use toast?
+		if (TextUtils.isEmpty(mEmail)) {
+			Context context = getApplicationContext();
+			CharSequence text = "Please Insert Email";
+			int duration = Toast.LENGTH_SHORT;
+			Toast toast = Toast.makeText(context, text, duration);
+			toast.show();
+		} else if (!mEmail.contains("@")) {
+			Context context = getApplicationContext();
+			CharSequence text = "Error Invalid Email!";
+			int duration = Toast.LENGTH_SHORT;
+			Toast toast = Toast.makeText(context, text, duration);
+			toast.show();
+		} else {
+			// todo use shared prefrence as a bool to say you are logged in instead of a instance variable
+			isLoggedIn = true;
+		}
+		// todo what if user is not registered ? add another elif	
+
+		
+	}
+	
+	public void tryRegister(View view) {
+
+		mEmail = EmailView.getText().toString();
+		mPassword = PasswordView.getText().toString();
+				
+		attemptRegister();
+		/* todo register the person here */
+		if (isValid == true) {
+			Context context = getApplicationContext();
+			CharSequence text = "You are now registered";
+			int duration = Toast.LENGTH_SHORT;
+			Toast toast = Toast.makeText(context, text, duration);
+			toast.show();
+			
+		}
+		
+		// start another activity if the person succeed to login
+
+	}
+	
+	public void attemptRegister() {
+		if (TextUtils.isEmpty(mEmail)) {
+			Context context = getApplicationContext();
+			CharSequence text = "Please Insert Email";
+			int duration = Toast.LENGTH_SHORT;
+			Toast toast = Toast.makeText(context, text, duration);
+			toast.show();
+		} else if (!mEmail.contains("@")) {
+			Context context = getApplicationContext();
+			CharSequence text = "Error Invalid Email!";
+			int duration = Toast.LENGTH_SHORT;
+			Toast toast = Toast.makeText(context, text, duration);
+			toast.show();		
+		} else {
+			isValid = true;
+		}
+		
+	}
+	
+	public void storeCredentials() {
+		Editor editor = sharedpreferences.edit();
+	    editor.putString(EMAIL, mEmail);
+	    editor.commit();
+	}
+	
+}
---
+++ src/com/example/lunchamigomvp/ShakerActivity.java
@@ -0,0 +1,116 @@
+package com.example.lunchamigomvp;
+
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.lunchamigomvp.ShakeDetector.OnShakeListener;
+
+public class ShakerActivity extends ActionBarActivity {
+	
+	/* initialize the class */
+	private ShakeDetector mShakeDetector;
+	private SensorManager mSensorManager;
+	private Sensor mAccelerometer;
+	private Button bananaButton;
+	private Boolean firstTime; /* prevent toomuch timer */
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_shaker);
+
+		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		bananaButton = (Button) findViewById(R.id.bananaButton);
+		firstTime = true;
+		mShakeDetector = new ShakeDetector(new OnShakeListener() {
+			
+			@Override
+			public void onShake() {
+				// TODO Auto-generated method stub
+				if (firstTime == true) {
+					findFriends();
+				}
+			}
+		});
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.shaker, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		mSensorManager.registerListener(mShakeDetector, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+		firstTime = true;
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+		mSensorManager.unregisterListener(mShakeDetector);
+
+	}
+	
+	public void bananaButton(View view) {
+		if (firstTime == true) {
+			findFriends();
+		}
+	}
+	
+	/* todo server stuff */
+	public void findFriends() {
+		
+		CountDownTimer cdt = new CountDownTimer(3000, 1000) {
+
+			public void onTick(long millisUntilFinished) {
+				bananaButton.setBackgroundResource(R.drawable.banana_half);
+			}
+
+			public void onFinish() {
+				bananaButton.setBackgroundResource(R.drawable.banana);
+				startIntent();
+			}
+
+		}.start();
+		
+		firstTime = false;
+
+	}
+	
+	/* start the next intent */
+	public void startIntent() {
+		Intent intent = new Intent(this, ContactsActivity.class);
+		startActivity(intent);
+	}
+	
+	
+
+}
