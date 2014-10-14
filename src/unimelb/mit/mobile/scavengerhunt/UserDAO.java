578adff49fdce02e85b58fd749248b3befe90369
1vand1ng0 <ivansanchezvera@gmail.com>
2014-10-14 21:34:17 +1100

    "Commited Message View"

--- src/unimelb/mit/mobile/scavengerhunt/UserDAO.java
+++ src/unimelb/mit/mobile/scavengerhunt/UserDAO.java
@@ -36,7 +36,7 @@ public class UserDAO {
 	protected void connectDatabase() {
 		try{
 		//dbClient = new CouchDbClientAndroid("localx.properties");
-		dbClient = new CouchDbClientAndroid("raw/db/production.properties");
+		dbClient = new CouchDbClientAndroid("raw/db/localx.properties");
 		}catch(Exception e){
 			e.printStackTrace();
 		}
